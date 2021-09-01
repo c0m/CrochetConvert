62d4579ea56e6165b67254110aeb6f950372fcc3
c0m <61923663+c0m@users.noreply.github.com>
2021-09-01 18:48:39 +0100

    Add incredibly basic proof of concept
    Needs regex etc to be reliable. It works on a very basic level

--- CrochetConvert/Pages/Index.razor.cs
+++ CrochetConvert/Pages/Index.razor.cs
@@ -1,23 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
 namespace CrochetConvert.Pages
 {
+    public static class StringExtension
+    {
+        public static string UStoUK(this string s)
+        {
+            return new StringBuilder(s)
+                //abbr replacement
+                  .Replace("dc", "sc")
+                  .Replace("sl st", "ss")
+                  .Replace("htr", "hdc")
+                  .Replace("dtr", "tr")
+                  .Replace("ttr", "tr")
+                  .Replace("tr", "dc")
+                  .ToString();
+        }
+
+        public static string UKtoUS(this string s)
+        {
+            return new StringBuilder(s)
+                  //abbr replacement
+                  .Replace("sc", "dc")
+                  .Replace("ss", "sl st")
+                  .Replace("hdc", "htr")
+                  .Replace("tr", "dtr")
+                  .Replace("tr", "ttr")
+                  .Replace("dc", "tr")
+                  .ToString();
+        }
+    }
     public partial class Index : ComponentBase
     {
         string OutputBoxText { get; set; } = string.Empty;
+        string InputBoxText { get; set; } = string.Empty;
 
         private void CreateOutputText(int mode)
         {
             //since there are only two ways this can go we'll just if/else
-            if(mode == 0) //US mode
+            if(mode == 0) //UK to US mode
             {
                 OutputBoxText = ConvertToUS();
             }
-            else //UK mode
+            else //US to UK mode
             {
                 OutputBoxText = ConvertToUK();
             }
@@ -25,12 +55,12 @@ namespace CrochetConvert.Pages
 
         private string ConvertToUS()
         {
-            return "UStest";
+            return InputBoxText.UKtoUS();
         }
 
         private string ConvertToUK()
         {
-            return "UKtest";
+            return InputBoxText.UStoUK();
         }
     }
 }
